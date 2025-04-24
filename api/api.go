1ba082234a9fc9ccdd8d5ea163119fe015ffbc98
JasnRathore <139693551+JasnRathore@users.noreply.github.com>
2025-04-24 21:26:26 +0530

    Cleaning up Project structure

---
+++ api/api.go
@@ -0,0 +1,142 @@
+package api
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	
+	"fmt"
+    "log"
+	"os"
+	"path/filepath"
+	"encoding/json"
+)
+
+type Alias struct {
+    ID   int `json:"id"`
+    Name  string `json:"name"`
+	Location string `json:"location"`
+}
+
+type GetResponse struct {
+	Status bool `json:"status"`
+	Location string `json:"location"`
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getPath() string {
+	exePath, err := os.Executable()
+	check(err)
+	dir := filepath.Dir(exePath)
+	return dir + "/aliases.db"
+}
+
+func initDatabase() {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+	
+	createTable := ` 
+	CREATE TABLE IF NOT EXISTS aliases (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL UNIQUE,
+		location TEXT NOT NULL UNIQUE
+	);`
+	
+	_, err = db.Exec(createTable)
+	check(err)
+}
+
+func InsertData(name string, location string) {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+	
+	insertAlias := `INSERT INTO aliases (name, location) VALUES (?,?);`
+	result, err := db.Exec(insertAlias, name, location)
+	check(err)
+	
+	userID, _ := result.LastInsertId()
+	fmt.Printf("Sucessfuly inserted Alias\nID: %d\nName: %s\nLocation:%s\n", userID,name,location);
+}
+
+func GetAlias(name string) {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+
+	alias := Alias{}
+	err = db.QueryRow("SELECT id, name, location FROM aliases WHERE name = ?", name).
+		Scan(&alias.ID, &alias.Name, &alias.Location)
+	check(err)
+	response := GetResponse {
+		Status: true,
+		Location: alias.Location,
+	}
+	json.NewEncoder(os.Stdout).Encode(response)
+}
+
+func DeleteData(name string) {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+	
+	stmt, err := db.Prepare("DELETE FROM aliases WHERE name = ?")
+	check(err)
+	defer stmt.Close()
+
+	// Execute the statement with the provided name
+	result, err := stmt.Exec(name)
+	check(err)
+	
+	rowsAffected, err := result.RowsAffected()
+	check(err)
+	
+	if rowsAffected > 0 {
+		fmt.Println("Row deleted successfully.")
+	} else {
+		fmt.Println("No row found with that name.")
+	}	
+}
+
+func CheckAlias(name string) {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+	
+	var exists int
+	err = db.QueryRow("SELECT 1 FROM aliases WHERE name = ? LIMIT 1", name).Scan(&exists)
+	
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("false")
+		} else {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("true")
+	}
+}
+
+func ListAliases() {
+	db, err := sql.Open("sqlite3", getPath())
+	check(err)
+	defer db.Close()
+	
+	rows, err := db.Query("SELECT id, name, location FROM aliases")
+	check(err)
+    defer rows.Close()
+
+    var aliases []Alias
+    for rows.Next() {
+        var u Alias
+	    err := rows.Scan(&u.ID, &u.Name, &u.Location)
+		check(err)
+        aliases = append(aliases, u)
+    }
+	json.NewEncoder(os.Stdout).Encode(aliases)
+}
